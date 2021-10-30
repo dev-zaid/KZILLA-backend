e5914bcbc983223d3b8a6a58daa666efbe0f7641
Mohd Zaid <zaid.tab123@gmail.com>
2021-10-30 13:58:12 +0530

    ✅ Initiated server

--- task_29-10-2021/index.js
+++ task_29-10-2021/index.js
@@ -6,6 +6,10 @@ const port = 5000;
 const server = http.createServer((req,res) => {
     console.log(req.header);
     res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log("server running at http://127.0.0.1:5000/")
 });
