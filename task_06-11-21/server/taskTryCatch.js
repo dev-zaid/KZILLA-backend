04703d109a335ff93e290fb11fdd564bc2c5fde9
Mohd Zaid <zaid.tab123@gmail.com>
2021-11-09 15:32:18 +0530

    ✨ Completed task - thenCatch

--- task_06-11-21/server/taskTryCatch.js
+++ task_06-11-21/server/taskTryCatch.js
@@ -1,1 +1,11 @@
 const {generator} = require("./task.js");
+
+const noOfPromises = 15;
+const promiseArray = generator(noOfPromises); //promises are stored in promiseArray
+
+for(let i=0; i<noOfPromises; i++){
+    const promise = promiseArray[i];
+    promise
+        .then((values) => console.log(values))
+        .catch((error) => console.log("Error "+ error));
+}
