eb33e6edd03f90a3f5d2efb5254a8a708be63a29
Mohd Zaid <zaid.tab123@gmail.com>
2021-11-14 16:44:23 +0530

    ✨ Completed Promises task

--- task_06-11-21/server/taskAwait.js
+++ task_06-11-21/server/taskAwait.js
@@ -1,4 +1,21 @@
 const {generator} = require("./task");
 
-const promiseArray = generator(15);
+const noOfPromises = 15;
+const promiseArray = generator(noOfPromises); //promises are stored in promiseArray
+
+const taskAwait = async() => {
+    
+    for(let i=0; i<noOfPromises; i++){
+        const promise = promiseArray[i];
+        try {    
+            const values = await promise;
+            console.log(values);
+        }catch (error) {
+        console.log("Error "+error)
+    }
+}
+    
+}
+
+taskAwait();
 
